134d3dc86339cc39861ab81c51c59f48f80f0a6d
unknown <melissavanbussel@trentu.ca>
2019-03-27 18:44:12 -0400

    Fix database connection problems
    - Fix problem in file path

--- CCA-Java/src/kiosk/Controller.java
+++ CCA-Java/src/kiosk/Controller.java
@@ -5,47 +5,6 @@ import java.util.ArrayList;
 
 public class Controller {
 
-    public static Connection connectToDB() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:sqlite:menu.db");
-            Statement statement = conn.createStatement();
-        } catch (SQLException e){
-            System.out.println("Something went wrong: " + e.getMessage());
-        }
-        return conn;
-    }
 
-    public static Statement getStatement(Connection conn) {
-        Statement statement = null;
-        try {
-            statement = conn.createStatement();
-        } catch (SQLException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
-        }
-        return statement;
-    }
-
-    public static void closeConnection(Statement statement, Connection conn) {
-        try {
-            statement.close();
-            conn.close();
-        } catch (SQLException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
-        }
-
-    }
-    public static ArrayList<String> generateTypes(Statement statement){
-        ArrayList<String> types = new ArrayList<>();
-        try {
-            ResultSet rs = statement.executeQuery("SELECT DISTINCT type FROM menu");
-            while (rs.next()) {
-                types.add(rs.getString("type"));
-            }
-        } catch (SQLException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
-        }
-        return types;
-    }
 
 }
--- CCA-Java/src/kiosk/Main.java
+++ CCA-Java/src/kiosk/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main extends Application {
 
     @Override
@@ -18,6 +21,58 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        // launch(args);
+        ArrayList<String> types = generateTypes();
+        for (String t : types) {
+            System.out.println(t);
+        }
+       // closeConnection(statement, conn);
+    }
+
+    public static Connection connectToDB() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:src\\menu.db");
+        } catch (SQLException e){
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+        return conn;
+    }
+
+    public static Statement getStatement(Connection conn) {
+        Statement statement = null;
+        try {
+            statement = conn.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+        return statement;
+    }
+
+    public static void closeConnection(Statement statement, Connection conn) {
+        try {
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+
+    }
+    public static ArrayList<String> generateTypes(){
+        ArrayList<String> types = new ArrayList<>();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:src\\menu.db");
+            System.out.println(conn);
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT DISTINCT type FROM menu");
+            while (rs.next()) {
+                types.add(rs.getString("type"));
+            }
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+        return types;
     }
 }
