04da7a45d6d334328442b9eb68857b4fd6b1d6bf
unknown <melissavanbussel@trentu.ca>
2019-03-30 22:53:25 -0400

    Add order table to home screen

--- CCA-Java/src/kiosk/HomeController.java
+++ CCA-Java/src/kiosk/HomeController.java
@@ -1,16 +1,26 @@
 package kiosk;
 
+import javafx.fxml.Initializable;
 import javafx.scene.layout.*;
 import javafx.fxml.FXML;
 
-public class HomeController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
     /* Dimensions for VBox */
     public static final int PREV_HEIGHT = 170; //Preview Height
     public static final int WIDTH = Main.HALF_WIDTH; //Width of the Side-Panel
     public static final int HEIGHT = Main.HEIGHT - PREV_HEIGHT; //Height of the VBox
 
     @FXML AnchorPane root;
+    static public VBox dick;
     static AnchorPane sideBar;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        root.getChildren().add(dick);
+    }
 
     @FXML
     public void openSideMenu() {
