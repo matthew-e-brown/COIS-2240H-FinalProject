613c2812e133fd3b45088c00dba24d9de2934dc2
Melissa Van Bussel <melissavanbussel@trentu.ca>
2019-04-05 19:55:41 -0400

    Add documentation for HomeController, CategoryController

--- CCA-Java/src/kiosk/CategoryController.java
+++ CCA-Java/src/kiosk/CategoryController.java
@@ -22,29 +22,70 @@ import java.util.ResourceBundle;
 import static kiosk.Main.order;
 
 public class CategoryController implements Initializable {
+    /**
+     * The AnchorPane which contains all of the other nodes.
+     */
     @FXML AnchorPane root;
+    /**
+     * The button which allows the user to open the navigation drawer.
+     */
     @FXML Button hamburger;
+    /**
+     * The button which allows the user to view their order, by switching to the order screen.
+     */
     @FXML Button orderButton;
+    /**
+     * The grid which contains the 6 categories
+     */
     @FXML GridPane typesGrid;
+    /**
+     *
+     */
     @FXML Pane numberPane;
+    /**
+     * Contains the number of food or drink items currently in the user's order.
+     */
     @FXML Text orderNumber;
+    /**
+     * The image at the top of the screen.
+     */
     @FXML ImageView banner;
 
+    /**
+     * Each instance of CategoryController corresponds to one of 6 categories, given by the <code>category</code> field.
+     */
     private String category;
+
+    /**
+     * The Anchor Pane which contains the sidebar / navigation drawer.
+     */
     static AnchorPane sideBar;
 
+    /**
+     * This constructor is used to create an instance of
+     * CategoryController for each category.
+     * @param category The food category (e.g., Salad, Burgers, etc)
+     */
     CategoryController(String category) { this.category = category; }
 
+    /**
+     * Initializes the Category screen for the given category.
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Set the banner image for the specific category
         banner.setImage(new Image("file:src/img/Banners/" + this.category + ".png"));
 
+        // Set spacing between nodes in the types grid
         typesGrid.setVgap(15);
         typesGrid.setHgap(15);
 
         try {
             if (this.category == null) throw new Exception("Type must be set before initialize is run");
             int i = 0, j = 0;
+            // Load each item in the given category
             for (String item : Menu.getItemsByType(this.category)) {
                 // Load the category item
                 CategoryItemController controller = new CategoryItemController(item, this);
@@ -52,10 +93,12 @@ public class CategoryController implements Initializable {
                 loader.setController(controller);
                 StackPane itemPane = loader.load();
 
+                // Display the name of the item on the button
                 controller.button.setText(controller.itemName);
+                // When the user clicks the button, add the item to the user's order
                 controller.button.setOnAction((event) -> controller.addToOrder());
 
-                //Put a image URL
+                // Set the image for the item
                 String path = Menu.getFilepath(item);
                 controller.setImageURL(path);
 
@@ -67,30 +110,45 @@ public class CategoryController implements Initializable {
             }
         } catch (Exception e) { e.printStackTrace(); }
         hamburger.setOnAction((event) -> openSideMenu());
-        //Make a circle for the number;
+        //Make a circle which will display the number of items currently in the user's order
         double width = numberPane.getPrefWidth();
         numberPane.setPrefHeight(width);
         numberPane.setClip(new Circle(width / 2, width / 2, width / 2));
 
+        // Display the number of items in the order if the order length is greater than 1
         int orderLength = order.getLength();
         if (orderLength > 0) {
             numberPane.setVisible(true);
             orderNumber.setText(String.valueOf(orderLength));
         }
 
+        // When the user clicks the "View Order" button, go to the order screen
         orderButton.setOnAction(event -> viewOrder());
     }
 
+    /**
+     * Opens the navigation drawer. This method is called when the user presses
+     * the hamburger button.
+     */
     private void openSideMenu() {
         root.getChildren().add(sideBar);
     }
 
+    /**
+     * Increases the number in the red circle on top of the "View Order" button.
+     * This circle displays the number of items currently in the user's order.
+     * This method is invoked any time a user adds to their order.
+     */
     public void incrementOrderIcon() {
         if (!numberPane.isVisible()) numberPane.setVisible(true); //Show it if it doesn't exist
         int n = Integer.parseInt(this.orderNumber.getText());
         orderNumber.setText(String.valueOf(++n));
     }
 
+    /**
+     * Opens the order screen. This method is called when the user
+     * presses the "View Order" button.
+     */
     private void viewOrder() {
         Stage primaryStage = (Stage)root.getScene().getWindow();
         OrderController controller = Main.getOrderController();
--- CCA-Java/src/kiosk/HomeController.java
+++ CCA-Java/src/kiosk/HomeController.java
@@ -15,18 +15,55 @@ import java.util.Random;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
+    /**
+     * The height of the banner on each screen. This is equal
+     * to 1/4 of the height of the app.
+     */
     public static final int BANNER_HEIGHT = Main.QUARTER_HEIGHT;
+
+    /**
+     * The height of the scroll panes on the home screen and category screens.
+     */
     public static final int OBJECT_HEIGHT = (int)((Main.HEIGHT - BANNER_HEIGHT) * 0.85);
+
+    /**
+     * The side offset for objects on the home screen, category screens, and order screen.
+     */
     public static final int OBJECT_SIDE_OFFSET = (int)(Main.WIDTH * 0.05);
+
+    /**
+     * The Y-position of objects below banners.
+     */
     public static final int OBJECT_TOP_POS = BANNER_HEIGHT + OBJECT_SIDE_OFFSET;
+
+    /**
+     * The spacing between items in the grid containing the 6 different categories.
+     */
     public static final int GRID_GAP = 15;
 
+    /**
+     * The Anchor Pane which contains all of the other nodes, on the home screen.
+     */
     @FXML AnchorPane root;
+
+    /**
+     * The Grid Pane which contains the 6 different categories on the home screen.
+     */
     @FXML GridPane typesGrid;
+
+    /**
+     * The Anchor Pane which contains the sidebar / navigation drawer.
+     */
     static AnchorPane sideBar;
 
+    /**
+     * Initializes the home screen.
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Create instance of Random class in order to randomly choose an image for each category
         Random r = new Random();
         int i = 0, j = 0;
         try {
@@ -39,7 +76,7 @@ public class HomeController implements Initializable {
                 controller.button.setText(category);
                 controller.button.setOnAction((event) -> controller.selectCategory());
 
-                //Put a random image URL in there
+                // Randomly pick one of the items in the category and set the category image to an image of this item
                 ArrayList<String> items = Menu.getItemsByType(category);
                 String path = Menu.getFilepath(items.get(r.nextInt(items.size())));
                 controller.setImageURL(path);
@@ -53,6 +90,10 @@ public class HomeController implements Initializable {
         } catch (Exception e) { e.printStackTrace(); }
     }
 
+    /**
+     * Opens the navigation drawer. This method is called when the user presses
+     * the hamburger button.
+     */
     @FXML
     public void openSideMenu() {
         root.getChildren().add(sideBar);
